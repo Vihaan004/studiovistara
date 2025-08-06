9c06d85a60f1ca7023da64f68decaae793e345e8
Vihaan <vihaan004@gmail.com>
2025-08-06 13:07:29 +0530

    asset paths

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  basePath: isProd && isGitHubPages ? '/studiovistara' : '',
+  assetPrefix: isProd && isGitHubPages ? '/studiovistara/' : '',
 };
 
 export default nextConfig;
