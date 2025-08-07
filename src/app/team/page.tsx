40c34d311ea309fa51d630f8beedabf6949d731f
Vihaan <vihaan004@gmail.com>
2025-08-07 20:53:18 +0530

    team

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -10,6 +10,77 @@ export default function TeamPage() {
       <Poster />
       <div className="page-content">
         {/* Team page content will go here */}
+        <div className='team-wrapper'>
+          <div className='grid'>
+            
+          </div>
+
+
+
+          <div className='team'>
+            <div className='role-block'>
+              <div className='role'>FOUNDING ARCHITECT</div>
+              <div className='members'>
+                <p>INDRAKANT PATEL</p>
+              </div>
+            </div>
+
+            <div className='role-block'>
+              <div className='role'>PRINCIPAL ARCHITECT</div>
+              <div className='members'>
+                <p>TEJAS PATEL</p>
+              </div>
+            </div>
+
+            <div className='role-block'>
+              <div className='role'>PRINCIPAL INTERIOR DESIGNER</div>
+              <div className='members'>
+                <p>MONA PATEL</p>
+              </div>
+            </div>
+
+            <div className='role-block'>
+              <div className='role'>SENIOR ENGINEERS</div>
+              <div className='members'>
+                <p>JIGNESH SHAH</p>
+                <p>ROHIT HINGURAO</p>
+                <p>GOVIND DESAI</p>
+              </div>
+            </div>
+
+            <div className='role-block'>
+              <div className='role'>SENIOR ARCHITECT</div>
+              <div className='members'>
+                <p>MAYANK DUBEY</p>
+              </div>
+            </div>
+
+            <div className='role-block'>
+              <div className='role'>JUNIOR ARCHITECTS</div>
+              <div className='members'>
+                <p>NIMISHA SIHOTA</p>
+                <p>RINKAL KARAVADIA</p>
+                <p>VISHAL KHIMYANI</p>
+              </div>
+            </div>
+
+            <div className='role-block'>
+              <div className='role'>INTERNS</div>
+              <div className='members'>
+                <p>DHRUVI THAKKER</p>
+              </div>
+            </div>
+
+            <div className='role-block'>
+              <div className='role'>PEN-PUSHER</div>
+              <div className='members'>
+                <p>SANTOSH CHANDRATRE</p>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
       </div>
     </div>
   );
